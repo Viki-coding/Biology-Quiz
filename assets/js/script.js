a3151ec1d3db70be9ffad8cd287736cc44bd3ff4
Viki Mulhall <vikimulhall@gmail.com>
2024-08-26 09:45:56 +0000

    Add JS comments to understand what code is required

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,22 @@
 // comments //
+// Event Listener for DOM to start quiz once all loaded//
+// Event Listener – has player entered name?// 
+// Event Listener  - Load questions once player has entered name and clicked on START// 
+// Event Listener  - Load next questions once player has selected an answer and clicked on NEXT// 
+// Event Listener – Load next questions if player clicks PLAY AGAIN unless they have reached 10th question// 
+// Event Listener – Once the users has reached 10th question – display the REULTS page with score// 
+// Event Listener – Return user to HOME screen if they click on HOME// 
+
+// Functions: // 
+// Username – user to enter name then START – save user name to user on RESULTS page// 
+// START Quiz // 
+// NEXT question till 10th question then end quiz to display// 
+// RETULTS // 
+// Display Number of Questions – questions will increase // 
+// Display correct questions // 
+// Display current question// 
+
+// Remove previous answers when user clicks on NEXT and display next questions// 
+// Is the question correct? True or False// 
+// Display RESULTS score and final message// 
+// If user clicks replay – reset score and start on question 11-20.// 
