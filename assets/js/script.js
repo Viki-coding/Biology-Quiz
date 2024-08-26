0fcedb3c265dae3d8e2c2ac5f210cc6adc42b758
Viki Mulhall <vikimulhall@gmail.com>
2024-08-26 10:09:21 +0000

    update quiz box styling and correct some spellings in comments in js comments file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,17 @@
 // comments //
 // Event Listener for DOM to start quiz once all loaded//
-// Event Listener – has player entered name?// 
-// Event Listener  - Load questions once player has entered name and clicked on START// 
-// Event Listener  - Load next questions once player has selected an answer and clicked on NEXT// 
-// Event Listener – Load next questions if player clicks PLAY AGAIN unless they have reached 10th question// 
+// Event Listener – has player entered NAME?// 
+// Event Listener  - Load questions once player has ENTERED NAME and clicked on START// 
+// Event Listener  - Load NEXT questions once player has selected an ANSWER and clicked on NEXT// 
 // Event Listener – Once the users has reached 10th question – display the REULTS page with score// 
+// Event Listener – Load NEXT questions if player clicks PLAY AGAIN unless they have reached 10th question// 
 // Event Listener – Return user to HOME screen if they click on HOME// 
 
 // Functions: // 
-// Username – user to enter name then START – save user name to user on RESULTS page// 
+// Username – user to enter name then START – save user name to user so we can display on RESULTS page// 
 // START Quiz // 
-// NEXT question till 10th question then end quiz to display// 
-// RETULTS // 
+// NEXT question till 10th question then end quiz to display score// 
+
 // Display Number of Questions – questions will increase // 
 // Display correct questions // 
 // Display current question// 
