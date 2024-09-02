caeea0620636e7f66cac4adbdc8224583b6fb5b7
Viki Mulhall <vikimulhall@gmail.com>
2024-09-02 11:49:51 +0000

    add score++ to selectAnswer correct to increasescore by 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -232,12 +232,12 @@ let nextButton = document.getElementById("next-btn");
 
 //Question number and score changing once we start quiz - create variables to restore question index and score//
 let currentQuestionIndex = 0;
-let score = 0; 
+let score = 0;
 
 //Function to start quiz -> reset current question and score to begin at 0//
 // -> reset next.Button //
 // -> call the display question function//
-function startQuiz(){
+function startQuiz() {
     currentQuestionIndex = 0
     score = 0;
     nextButton.innerHTML = "Next";
@@ -248,21 +248,21 @@ function startQuiz(){
 // set the question number incrementally//
 //display the various questions using the question element that we have id in the html//
 //resetState to hide the Ansewer 1, 2, 3, 4 then //
-function displayQuestion(){
+function displayQuestion() {
     resetState();
-    let currentQuestion = questions [currentQuestionIndex];
-    let questionNum = currentQuestionIndex +1;
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNum = currentQuestionIndex + 1;
     questionElement.innerHTML = questionNum + ". " + currentQuestion.question;
-    
 
-//display potential answers for each question in the buttons created in html//
-//add event listerner so that when user clicks on answer it will be recoginized//
+
+    //display potential answers for each question in the buttons created in html//
+    //add event listerner so that when user clicks on answer it will be recoginized//
     currentQuestion.answers.forEach(answers => {
         let button = document.createElement('button');
-        button.innerHTML= answers.text;
+        button.innerHTML = answers.text;
         button.classList.add("btn");
         answerButtons.appendChild(button);
-        if(answers.correct){
+        if (answers.correct) {
             button.dataset.correct = answers.correct;
         }
         button.addEventListener("click", selectAnswer);
@@ -270,29 +270,32 @@ function displayQuestion(){
 
 }
 //define function to resetState do the previous answers(firstChild) are not diplayed//
-function resetState(){
+function resetState() {
     nextButton.style.display = "none"
-    while(answerButtons.firstChild){
+    while (answerButtons.firstChild) {
         answerButtons.removeChild(answerButtons.firstChild);
     }
 }
 
 //When user clicks on the selected answer it will check in the dataset answers if the ans is correct or incorrect//
-    function selectAnswer(e){
-        const selectedBtn = e.target;
-        const isCorrect = selectedBtn.dataset.correct === "true";
-        if(isCorrect){
-            selectedBtn.classList.add("correct");
-        }else{
-            selectedBtn.classList.add("incorrect");
+// add an array from each dataset to check if the answer is true or not - it will change color to green and then it will be disabled to select another answer, the next button then displayed once and answer selected//
+//add score ++ so the increase the score by 1 
+function selectAnswer(e) {
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if (isCorrect) {
+        selectedBtn.classList.add("correct");
+        score++;
+    } else {
+        selectedBtn.classList.add("incorrect");
+    }
+    Array.from(answerButtons.children).forEach(button => {
+        if (button.dataset.correct === "true") {
+            button.classList.add("correct");
         }
-        Array.from(answerButtons.children).forEach(button => {
-            if(button.dataset.correct === "true"){
-                button.classList.add("correct");
-            }
-            button.disabled = true;
-        });
-        nextButton.style.display = "block";
+        button.disabled = true;
+    });
+    nextButton.style.display = "block";
 }
 
-    startQuiz()
+startQuiz()
