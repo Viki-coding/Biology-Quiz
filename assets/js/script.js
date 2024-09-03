5a69545a9e2aa5ab5636a61dd089ff82fcc04c13
Viki Mulhall <vikimulhall@gmail.com>
2024-09-03 11:26:28 +0000

    fix incorrect spelling in html and delete guidence comments in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,4 @@
-// Event Listener for DOM to start quiz once all loaded//
 
-
-// Event Listener – has player entered NAME?// 
-// Event Listener  - Load questions once player has ENTERED NAME and clicked on START// 
-// Event Listener  - Load NEXT questions once player has selected an ANSWER and clicked on NEXT// 
-// Event Listener – Once the users has reached 10th question – display the REULTS page with score// 
-// Event Listener – Load NEXT questions if player clicks PLAY AGAIN unless they have reached 10th question// 
-// Event Listener – Return user to HOME screen if they click on HOME// 
-
-// Functions: // 
-// Username – user to enter name then START – save user name to user so we can display on RESULTS page// 
-// START Quiz // 
-// NEXT question till 10th question then end quiz to display score// 
-
-// Display Number of Questions – questions will increase // 
-// Display correct questions // 
-// Display current question// 
-
-// Remove previous answers when user clicks on NEXT and display next questions// 
-// Is the question correct? True or False// 
-// Display RESULTS score and final message// 
-// If user clicks replay – reset score and start on question 11-20.// 
 
 // let the variable of questions include the following list(array) of questions and answers//
 let questions = [{
